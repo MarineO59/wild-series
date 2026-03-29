29c4154125154a97505b3d172804e9dcac8c5022
Marine_O <marine.opsommer@gmail.com>
2026-03-29 20:47:51 +0200

    feat: add program and category repositories

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,28 +1,14 @@
-const categories = [
-  {
-    id: 1,
-    name: "Comédie",
-  },
-  {
-    id: 2,
-    name: "Science-Fiction",
-  },
-];
+import categoryRepository from "./categoryRepository";
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
-  const parsedId = Number.parseInt(req.params.id);
-  const category = categories.find((c) => c.id === parsedId);
-  if (category != null) {
-    res.json(category);
-  } else {
-    res.sendStatus(404);
-  }
+  res.sendStatus(404);
 };
 
 export default { browse, read };
