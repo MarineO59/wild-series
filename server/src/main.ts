ba593dfb34ce46047cfc4da0ffdbe307c81ff960
Marine_O <marine.opsommer@gmail.com>
2026-03-29 19:44:36 +0200

    monorepo02 done

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,16 @@
-// Load environment variables from .env file
 import "dotenv/config";
-
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
 import "../database/checkConnection";
-
-// Import the Express application from ./app
+import type { RequestHandler } from "express";
 import app from "./app";
 
-// Get the port from the environment variables
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
+
 const port = process.env.APP_PORT;
 
-// Start the server and listen on the specified port
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
